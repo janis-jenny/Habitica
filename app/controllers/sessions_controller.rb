e2df0aab118564f47e79ce287c5d038b7b365abb
janis-jenny <pcardenasvm@gmail.com>
2021-01-25 17:58:32 -0500

    add sign up page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(params[:name])
+    user = User.find_by(name: params[:name])
     if user
       session[:user_id] = user.id
       redirect_to root_path
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,16 @@
 module UsersHelper
+  def user_params
+    params.require(:user).permit(:name)
+  end
+
+=begin   def user_errors
+    return unless @user.errors.any?
+
+    @user.errors.full_messages.each do |msg|
+      if msg.include?('Name')
+        @uclas1 = 'border border-danger'
+      end
+    end
+  end 
+=end
 end
