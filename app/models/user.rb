5fe1f74335e33fd3ff8736ba58ef9bd98d6ce129
janis-jenny <pcardenasvm@gmail.com>
2021-01-28 11:14:26 -0500

    edit the user model and the habit list

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :habits
   has_many :groups
   has_one_attached :avatar
-  validates :name, presence: true
-  validates :name, uniqueness: true
+=begin   validates :name, presence: true
+  validates :name, uniqueness: true 
+=end
 end
