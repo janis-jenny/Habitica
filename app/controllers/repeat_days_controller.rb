58c1f172bde47540214da3a9e805bd1acd962272
janis-jenny <pcardenasvm@gmail.com>
2021-02-09 11:37:55 -0500

    add  style to habits show and new

--- app/controllers/repeat_days_controller.rb
+++ app/controllers/repeat_days_controller.rb
@@ -10,10 +10,9 @@ class RepeatDaysController < ApplicationController
   def destroy
     @repeat_days = RepeatDay.find_by(day: params[:day], user_id: current_user.id)
     if @repeat_days.delete
-      #flash[:success] = 'Vote was successfully deleted.'
+      flash[:success] = 'Day was successfully deleted.'
     else
-      #flash[:error] = 'Something went wrong'
+      flash[:error] = 'Something went wrong'
     end
-  
   end
 end
