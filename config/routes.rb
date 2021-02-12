7c2020e746e99639018da31380b2cf71ae531101
janis-jenny <pcardenasvm@gmail.com>
2021-02-12 16:39:20 -0500

    specify needed routes in the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users
-  resources :habits
-  resources :groups
-  resources :repeat_days
+  resources :users, only: [:index, :new, :create, :show, :update, :edit]
+  resources :habits, only: [:index, :new, :create, :show]
+  resources :groups, only: [:index, :new, :create, :show]
+  resources :repeat_days, only: [:create, :destory]
 
   get '/external_habits', to: 'habits#external_habits'
   get '/login', to: 'sessions#new'
