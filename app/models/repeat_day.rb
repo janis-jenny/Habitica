3edb04d0d5ced4d7f456471fb3f4686be2f08274
janis-jenny <pcardenasvm@gmail.com>
2021-02-12 20:49:33 -0500

    refactor helper select_days

--- app/models/repeat_day.rb
+++ app/models/repeat_day.rb
@@ -1,6 +1,4 @@
 class RepeatDay < ApplicationRecord
   belongs_to :habit
   belongs_to :user
-  validates :user_id, uniqueness: { scope: :day }
-  validates :habit_id, uniqueness: { scope: :day }
 end
