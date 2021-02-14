db6cc49648642c88d13738326e4269959da35822
janis-jenny <pcardenasvm@gmail.com>
2021-02-13 19:02:53 -0500

    add more tests

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
   gem 'rspec-rails'
+  # gem 'bullet'
 end
 
 group :development do
@@ -41,6 +42,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'hirb'
 end
 
 group :test do
@@ -49,9 +51,8 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of web drivers to run system tests with browsers
   gem 'webdrivers'
+  gem 'shoulda-matchers', '~> 4.0'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
-
-gem 'hirb'
--- spec/features/habit_spec.rb
+++ spec/features/habit_spec.rb
@@ -20,12 +20,29 @@ describe 'Habit', type: :feature do
     expect(page).to have_text('Welcome Jenny!')
   end
 
+  it 'when clicks on habits path you find a welcome text' do
+    user = User.last
+    visit "/users/#{user.id}"
+    expect(page).to have_link('Habits')
+    click_link 'Habits'
+    visit habits_path
+    expect(page).not_to have_text('Welcome Pao!')
+  end
+
   it 'when clicks on new habit path you find a Name text' do
     user = User.last
     visit habits_path
     click_on 'New Habit'
     visit '/habits/new'
     expect(page).to have_text('Name:')
+  end
+
+  it 'when clicks on new habit path you find a Name text' do
+    user = User.last
+    visit habits_path
+    click_on 'New Habit'
+    visit '/habits/new'
+    expect(page).not_to have_text('Nothing:')
   end
 
   it 'when clicks on habits path you find a welcome text' do
@@ -40,6 +57,50 @@ describe 'Habit', type: :feature do
     group = Group.last
     visit "/groups/#{group.id}"
     expect(page).to have_text('Group: Entertainment')
+  end
+
+  it 'when clicks on habits path you find a welcome text' do
+    user = User.last
+    visit "/users/#{user.id}"
+    click_on 'Group'
+    visit '/groups'
+    click_on 'New Group'
+    visit '/groups/new'
+    fill_in 'Name:', with: 'Entertainment'
+    find("input[type='submit']").click
+    group = Group.last
+    visit "/groups/#{group.id}"
+    expect(page).not_to have_text('Group: Self Care')
+  end
+end
+
+RSpec.describe Habit, type: :model do
+  describe 'associations' do
+    it { should have_many(:group_habits)}
+  end
+  describe 'associations' do
+    it { should belong_to(:user)}
+  end
+end
+
+RSpec.describe User, type: :model do
+  describe 'associations' do
+    it { should have_many(:habits)}
+  end
+  describe 'associations' do
+    it { should have_many(:groups)}
+  end
+end
+
+RSpec.describe Group, type: :model do
+  describe 'associations' do
+    it { should have_many(:group_habits)}
+  end
+  describe 'associations' do
+    it { should have_many(:habits)}
+  end
+  describe 'associations' do
+    it { should belong_to(:user)}
   end
 end
 # rubocop:enable Lint/UselessAssignment
