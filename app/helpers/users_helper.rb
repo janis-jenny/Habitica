469fdfb15c366459179131f4a5961a8408460b1b
janis-jenny <pcardenasvm@gmail.com>
2021-02-13 20:01:53 -0500

    add user's validations and alerts for errors

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,14 @@ module UsersHelper
   def user_icons(user)
     !user.avatar.attached? ? '/icon.png' : user.avatar
   end
+
+  def user_error
+    return unless @user.errors.any?
+
+    @user.errors.full_messages.each do |msg|
+      if msg.include?('Name')
+        @uclass = 'border border-danger'
+      end
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :habits
   has_many :groups
   has_one_attached :avatar
-  validates :name, presence: true
+  validates :name, presence: true, length: { in: 3..10 }
   validates :name, uniqueness: true
+  validates :name, format: { with: /[a-zA-Z0-9]/ }
 end
