a843e447c2c1115af27e3b6ca0f5cd2e32b226dc
janis-jenny <pcardenasvm@gmail.com>
2021-02-17 00:34:06 -0500

    change live version and config storage and gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,15 @@
 #   movies = H.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create(name: 'Jenny');
-User.create(name: 'Pao');
+User.create(name: 'User1');
 
 p User.all.count
 Habit.create(user_id: User.first.id, name: 'Habit1', time: 30);
-Group.create(name: '', user_id: User.first.id)
-GroupHabit.create(habit_id: Habit.first.id, group_id: Group.first.id)
-RepeatDay.create(day: 'Monday', habit_id: Habit.first.id, user_id: User.first.id)
+Group.create(name: '', user_id: User.first.id);
+GroupHabit.create(habit_id: Habit.first.id, group_id: Group.first.id);
+RepeatDay.create(day: 'Monday', habit_id: Habit.first.id, user_id: User.first.id);
 
 Habit.create(user_id: User.first.id, name: 'Habit2', time: 30);
-Group.create(name: 'Group1', user_id: User.first.id)
+Group.create(name: 'Mindfulness', user_id: User.first.id)
 GroupHabit.create(habit_id: Habit.second.id, group_id: Group.second.id)
 RepeatDay.create(day: 'Friday', habit_id: Habit.second.id, user_id: User.first.id)
